adc83f1846182d5704cb2329c56768817d78cef1
Sieg <siegblink@gmail.com>
2020-02-14 22:21:32 +0800

    Initial commit for the todo app.

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { StyleSheet, View} from 'react-native'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+})
